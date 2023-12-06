d4e7a501dbbc101d6290b3f5b36fff634c5df6ad
Anugya Jaiswal <139376690+anugyajais@users.noreply.github.com>
2023-12-06 23:04:29 +0530

    Files uploaded

---
+++ loginPage.js
@@ -0,0 +1,14 @@
+// javascript code !
+
+function abc(){
+    var x=document.getElementById("one").value;
+    var y=document.getElementById("two").value;
+    if(x==="cse326" && y==="k23mr")
+    {
+       window.alert("Login succesful !");
+    }
+    else{
+       window.alert("Login failed !");
+    }
+   }
+   
